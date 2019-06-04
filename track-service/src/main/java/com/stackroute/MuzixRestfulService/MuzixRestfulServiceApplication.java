a72a1b46ace32d1d7616beebe38241f1866210b6
Arathi <mj.arathi@gmail.com>
2019-06-04 10:32:17 +0530

    few changes

--- track-service/src/main/java/com/stackroute/MuzixRestfulService/MuzixRestfulServiceApplication.java
+++ track-service/src/main/java/com/stackroute/MuzixRestfulService/MuzixRestfulServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 @EnableEurekaClient
 public class MuzixRestfulServiceApplication {
@@ -18,34 +17,5 @@ public class MuzixRestfulServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MuzixRestfulServiceApplication.class, args);
 	}
-
-	/*@Bean
-	public CommandLineRunner demoData(TrackRepository trackRepo) {
-
-		Track track = new Track(5,"Photograph","Ed Sheeran");
-		Track track1 = new Track(7,"Shape of You","Ed Sheeran");
-
-		return args -> {
-			trackRepo.save(track);
-			trackRepo.save(track1);
-		};
-	}*/
-
 }
-
-//@RefreshScope
-//@RestController
-//class MessageRestController {
-////
-////		@Value("${msg:Hello world - Config Server is not working..pelase check}")
-////		private String msg;
-//
-//	@RequestMapping
-//	String getMsg() {
-//		String msg;
-//		msg = "stackroute";
-//		return msg;
-//
-//	}
-//}
 
--- track-service/src/main/java/com/stackroute/MuzixRestfulService/exceptions/ExceptionControllerAdvice.java
+++ track-service/src/main/java/com/stackroute/MuzixRestfulService/exceptions/ExceptionControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.stackroute.MuzixRestfulService.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -8,23 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler(TrackAlreadyExistsException.class)
-    public ResponseForException handleTrackAlreadyExistsException(TrackAlreadyExistsException trackAlreadyExistsException, final HttpServletRequest request) {
-
-        ResponseForException error = new ResponseForException();
-        error.setErrorMessage(trackAlreadyExistsException.getMessage());
-        error.setRequestedURI(request.getRequestURI());
-
-        return error;
-    }
-
+//    @ExceptionHandler(TrackAlreadyExistsException.class)
+//    public ResponseForException handleTrackAlreadyExistsException(TrackAlreadyExistsException trackAlreadyExistsException, final HttpServletRequest request) {
+//
+//        ResponseForException error = new ResponseForException();
+//        error.setErrorMessage(trackAlreadyExistsException.getMessage());
+//        error.setRequestedURI(request.getRequestURI());
+//
+//        return error;
+//    }
+//
+//    @ExceptionHandler(TrackNotFoundException.class)
+//    public ResponseForException handleTrackNotFoundException(TrackNotFoundException trackNotFoundException,final HttpServletRequest request) {
+//
+//        ResponseForException error = new ResponseForException();
+//        error.setErrorMessage(trackNotFoundException.getMessage());
+//        error.setRequestedURI(request.getRequestURI());
+//
+//        return error;
+//    }
     @ExceptionHandler(TrackNotFoundException.class)
-    public ResponseForException handleTrackNotFoundException(TrackNotFoundException trackNotFoundException,final HttpServletRequest request) {
-
-        ResponseForException error = new ResponseForException();
-        error.setErrorMessage(trackNotFoundException.getMessage());
-        error.setRequestedURI(request.getRequestURI());
-
-        return error;
+    public ResponseEntity handleMuzixNotFoundException(final TrackNotFoundException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
